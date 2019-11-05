df07b612573cfc1eef369af6223ee4dc3da16f9d
jeagues <54336695+jeagues@users.noreply.github.com>
2019-11-05 16:27:49 +0100

    Update Calculateur.java

--- src/main/java/com/formation/Calculateur.java
+++ src/main/java/com/formation/Calculateur.java
@@ -2,7 +2,7 @@ package com.formation;
 
 public class Calculateur {
 	public static int add(int a, int b) {
-		return a + b + 0 ;
+		return a + b + 0 + 0 ;
 	}
 	public static int multiply(int a , int b ) {
 		return a * b + 0 ;
