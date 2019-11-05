d7fb13d437cddf0329a47545f98e576bdd81d265
jhugues <jhugues.coindin@gmail.com>
2019-11-05 16:17:13 +0100

    init hello-test

---
+++ src/main/java/com/formation/Calculateur.java
@@ -0,0 +1,12 @@
+package com.formation;
+
+public class Calculateur {
+	public static int add(int a, int b) {
+		return a + b + 0 ;
+	}
+	public static int multiply(int a , int b ) {
+		return a * b + 0 ;
+	}
+
+}
+
